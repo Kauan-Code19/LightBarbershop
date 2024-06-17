92e23488cd2683cc8459a41f85398de1d8f72624
Kauan-Code19 <pkauprofissional@gmail.com>
2024-06-17 18:56:18 -0300

    projeto criado.

---
+++ src/main/java/com/kauan/LightBarbershop/LightBarbershopApplication.java
@@ -0,0 +1,13 @@
+package com.kauan.LightBarbershop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightBarbershopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LightBarbershopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kauan/LightBarbershop/LightBarbershopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kauan.LightBarbershop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LightBarbershopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
