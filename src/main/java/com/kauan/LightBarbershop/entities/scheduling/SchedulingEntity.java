bcbc5d8eda18c37a1353854e0afeb5212aef82ac
Kauan-Code19 <pkauprofissional@gmail.com>
2024-06-18 19:43:48 -0300

    criação da tabela Scheduling e as suas relações.

---
+++ src/main/java/com/kauan/LightBarbershop/entities/scheduling/SchedulingEntity.java
@@ -0,0 +1,51 @@
+package com.kauan.LightBarbershop.entities.scheduling;
+
+import com.kauan.LightBarbershop.entities.barber.BarberEntity;
+import com.kauan.LightBarbershop.entities.client.ClientEntity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GenerationType;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.EqualsAndHashCode;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.UUID;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Getter
+@Setter
+@Entity
+@Table(name = "scheduling")
+public class SchedulingEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", nullable = false, unique = true, updatable = false)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "barber_id", nullable = false)
+    private BarberEntity barber;
+
+    @OneToOne
+    @JoinColumn(name = "client_id", referencedColumnName = "id")
+    private ClientEntity client;
+
+    @Column(name = "date", nullable = false)
+    private LocalDate date;
+
+    @Column(name = "time", nullable = false)
+    private LocalTime time;
+}
